d56055520b6dd84c2a326c692a3e725cf775c514
UP230635 <up230635@alumnos.upa.edu.mx>
2026-06-19 19:07:38 -0600

    Tarea completa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,38 +20,38 @@ export default function Home() {
   const monstrarFrutas = () => {
     console.log(frutialumnos);
   }
- 
+
   return (
     <div>
       <h1>{title}</h1>
-        Hola 8B los saluda {name}
+      Hola 8B los saluda {name}
 
-        <br></br>
-        <br></br>
+      <br></br>
+      <br></br>
 
-        <button onClick={monstrarFrutas}>
-          Mostrar
-        </button>
+      <button onClick={monstrarFrutas}>
+        Mostrar
+      </button>
 
-        <br></br>
-        <br></br>
+      <br></br>
+      <br></br>
 
-        <ul>
-          {frutialumnos.map((fruta, index) => (
-            <li key={index}>
-              {fruta}
-            </li>
-          ))}
+      <ul>
+        {frutialumnos.map((fruta, index) => (
+          <li key={index}>
+            {fruta}
+          </li>
+        ))}
 
-          {frutialumnos.map((fruta) => {
-              let x = 0;
-            return(
+        {frutialumnos.map((fruta) => {
+          let x = 0;
+          return (
             <li>
               {fruta}
             </li>
-            )
-          })}
-        </ul>
+          )
+        })}
+      </ul>
     </div>
   );
 }
